605bd39a63c78fd81770452a892f6921d55164f2
brayan <brayanstivenp578@gmail.com>
2026-03-24 21:23:31 -0500

    feat: Implement UserController for user registration

---
+++ src/main/java/com/brayanspv/auth/controller/implementations/UserController.java
@@ -0,0 +1,30 @@
+package com.brayanspv.auth.controller.implementations;
+
+import com.brayanspv.auth.controller.contracts.IUserController;
+import com.brayanspv.auth.model.request.SignUpRequest;
+import com.brayanspv.auth.service.contracts.IUserService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@Log4j2
+public class UserController implements IUserController {
+
+
+    private IUserService userService;
+
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    @PostMapping(path = "signUp")
+    public Mono<ResponseEntity> signUp(SignUpRequest request) {
+        return Mono.just(request).flatMap(currentRequest -> {
+
+        })
+    }
+}
